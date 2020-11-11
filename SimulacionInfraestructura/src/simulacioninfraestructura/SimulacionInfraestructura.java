f0ac4cad0aed7d57af5bff55c70b4d5f06cd86ec
Francisco <franciscoecabanillas@gmail.com>
2020-11-11 20:07:00 -0300

    Inicializacion proyecto

---
+++ SimulacionInfraestructura/src/simulacioninfraestructura/SimulacionInfraestructura.java
@@ -0,0 +1,9 @@
+package simulacioninfraestructura;
+
+public class SimulacionInfraestructura {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
